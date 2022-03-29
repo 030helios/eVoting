85aa1b23638eeb1055a29c3e935d145d72f8cdb8
030helios <a95037@gmail.com>
2022-03-30 01:13:33 +0800

    added RegisterVoter to eVoting

--- server.py
+++ server.py
@@ -1,21 +1,34 @@
+import proto.vote_pb2_grpc as vote_grpc
+import proto.vote_pb2 as vote
 from concurrent import futures
 import logging
+from tokenize import group
 import grpc
 import sys
+
+from rsa import PublicKey
 sys.path.append('proto')
-import proto.vote_pb2 as vote
-import proto.vote_pb2_grpc as vote_grpc
 
 
 class eVoting(vote_grpc.eVotingServicer):
+    def RegisterVoter(self, request, context):
+        return vote.Voter(name='John', group='Student', PublicKey=b'\xDE\xAD')
+
+    def UnregisterVoter(self, request, context):
+        return vote.VoterName(name='John')
+
     def PreAuth(self, request, context):
         return vote.Challenge(value=b'\xDE\xAD')
+
     def Auth(self, request, context):
         return vote.AuthToken(value=b'\xDE\xAD')
+
     def CreateElection(self, request, context):
         return vote.ElectionStatus(code=1)
+
     def CastVote(self, request, context):
         return vote.VoteStatus(code=1)
+
     def GetResult(self, request, context):
         return vote.ElectionResult(status=1, count=0)
 
